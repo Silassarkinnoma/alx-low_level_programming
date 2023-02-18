6f1a458307ca53d83464ef893ce087f4ff237142
Silassarkinnoma <silassarkinnoma16@gmail.com>
2023-02-18 08:51:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - entry point 
+ * main - entry point
  *
  * Return: return 0 if successful or 1 if failed
  */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+puts("\"Programming is like building a multilingual puzzle");
+reture(0);
 }
