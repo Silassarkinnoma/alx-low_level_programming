382813c106cb2ea6ba27cb4ac6ba3e2fedfea36d
Silassarkinnoma <silassarkinnoma16@gmail.com>
2023-02-19 07:16:44 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,5 @@ printf("size of a int: %lu byte(s)", sizeof(int));
 printf("size of a long int: %lu byte(s)", sizeof(long int));
 printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 printf("size of a float: %lu byte(s)", sizeof(float));
-Return(0);
+Return (0);
 }
