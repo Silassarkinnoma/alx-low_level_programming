564ded984a233b9246c1905d17f6f5a2c8497d5d
Silassarkinnoma <silassarkinnoma16@gmail.com>
2023-02-19 06:49:43 -0800

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: return 1 if successful
+ */
+
+int main(void)
+{
+write(1,"\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 50)
+return (1);
+}
