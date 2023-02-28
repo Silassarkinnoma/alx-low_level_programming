c9fae8cd1db9c666ac8238f34d10154177052317
Silassarkinnoma <silassarkinnoma16@gmail.com>
2023-02-27 19:23:27 -0800

    string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *ng;
-goid swap_int(int *a, int *b);
-int _strlen(char *s);
+goid swap_int(int *a, int *bg;
+gnt _strlen(char *s);
+void _puts(char *str);
 
 #endif
