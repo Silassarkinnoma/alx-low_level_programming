e6bc346fab2c497966be44b027f9057a536f3de5
Silassarkinnoma <silassarkinnoma16@gmail.com>
2023-02-28 20:00:22 -0800

    string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints string
+ * @str: function parameter
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@ void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
 int _strlen(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif
